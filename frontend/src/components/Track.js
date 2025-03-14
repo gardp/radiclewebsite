75adb69050138d8eaba5a07de4ddac5e23cbb902
Gardly Philoctete <gardly.philoctete@gmail.com>
2025-03-14 12:40:58 -0400

    Make track icons clickable with links to stream, buy, and download

--- frontend/src/components/Track.js
+++ frontend/src/components/Track.js
@@ -3,38 +3,55 @@ import '../styles/AudioPlayer.css';
 import '../styles/Track.css';
 
 const Track = ({ track, isActive, onClick }) => {
+  // Function to handle track image click
+  const handleTrackImageClick = (e) => {
+    // Only trigger onClick if clicking directly on the track image
+    if (e.target.className === 'track-thumb') {
+      onClick();
+    }
+  };
+
   return (
     <div 
       className={`track-item ${isActive ? 'active' : ''}`}
       style={{ display: isActive ? 'none' : 'inline-block' }}
-      onClick={onClick}
     >
       <div className="track-thumb-container">
         <img
           className="track-thumb"
           src={track.image}
           alt={`track artwork for ${track.title}`}
+          onClick={handleTrackImageClick}
         />
         {track.links?.streamLink && (
-          <img
-            className="track-icon stream-icon"
-            src={`${process.env.PUBLIC_URL}/images/icons8-music-100.png`}
-            alt="Stream"
-          />
+          <a href={track.links.streamLink} target="_blank" rel="noopener noreferrer">
+            <img
+              className="track-icon stream-icon"
+              src={`${process.env.PUBLIC_URL}/images/icons8-music-100.png`}
+              alt="Stream"
+            />
+            <span className="tooltip">Stream</span>
+          </a>
         )}
         {track.links?.buyLink && (
-          <img
-            className="track-icon buy-icon"
-            src={`${process.env.PUBLIC_URL}/images/icons8-cart-100.png`}
-            alt="Buy"
-          />
+          <a href={track.links.buyLink} target="_blank" rel="noopener noreferrer">
+            <img
+              className="track-icon buy-icon"
+              src={`${process.env.PUBLIC_URL}/images/icons8-cart-100.png`}
+              alt="Buy"
+            />
+            <span className="tooltip">Buy</span>
+          </a>
         )}
         {track.links?.downloadLink && (
-          <img
-            className="track-icon download-icon"
-            src={`${process.env.PUBLIC_URL}/images/icons8-listening-to-music-on-headphones-100.png`}
-            alt="Download"
-          />
+          <a href={track.links.downloadLink} target="_blank" rel="noopener noreferrer">
+            <img
+              className="track-icon download-icon"
+              src={`${process.env.PUBLIC_URL}/images/icons8-listening-to-music-on-headphones-100.png`}
+              alt="Download"
+            />
+            <span className="tooltip">Download</span>
+          </a>
         )}
       </div>
       <h4 className="track-title">{track.title}</h4>
